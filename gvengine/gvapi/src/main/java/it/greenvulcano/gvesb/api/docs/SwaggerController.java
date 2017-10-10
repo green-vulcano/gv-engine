d1fbe6de48b94f91bf9aee3562391521971e4f07
rolag-it <rocco.lagrotteria@gmail.com>
2017-10-10 14:18:17 +0200

    Fix in SwaggerController

--- gvengine/gvapi/src/main/java/it/greenvulcano/gvesb/api/docs/SwaggerController.java
+++ gvengine/gvapi/src/main/java/it/greenvulcano/gvesb/api/docs/SwaggerController.java
@@ -1,19 +1,47 @@
+/*******************************************************************************
+ * Copyright (c) 2009, 2016 GreenVulcano ESB Open Source Project.
+ * All rights reserved.
+ *
+ * This file is part of GreenVulcano ESB.
+ *
+ * GreenVulcano ESB is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *  
+ * GreenVulcano ESB is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * GNU Lesser General Public License for more details.
+ *  
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with GreenVulcano ESB. If not, see <http://www.gnu.org/licenses/>.
+ *******************************************************************************/
 package it.greenvulcano.gvesb.api.docs;
 
+import java.io.IOException;
 import java.net.URI;
 import java.util.HashMap;
 import java.util.Map;
-
 import javax.ws.rs.GET;
 import javax.ws.rs.NotFoundException;
 import javax.ws.rs.Path;
 import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.core.Context;
 import javax.ws.rs.core.MediaType;
 import javax.ws.rs.core.Response;
 import javax.ws.rs.core.Response.ResponseBuilder;
+import javax.ws.rs.core.UriInfo;
+
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
+
+import com.fasterxml.jackson.core.JsonProcessingException;
+import com.fasterxml.jackson.databind.JsonNode;
+import com.fasterxml.jackson.databind.ObjectMapper;
+import com.fasterxml.jackson.databind.node.ObjectNode;
+import com.fasterxml.jackson.dataformat.yaml.YAMLMapper;
 
 public class SwaggerController {
 	
@@ -21,6 +49,9 @@ public class SwaggerController {
 	
 	private static final String UI_ROOT = "META-INF/resources/webjars/swagger-ui/3.2.2/"; 
     private static final Map<String, String> MEDIA_TYPES;
+    
+    private static final ObjectMapper OBJECT_MAPPER = new ObjectMapper();
+    private static final ObjectMapper YAML_MAPPER = new YAMLMapper();
     
 	static {
          MEDIA_TYPES = new HashMap<>();
@@ -36,27 +67,32 @@ public class SwaggerController {
          MEDIA_TYPES.put("woff2", "application/font-woff2");
     }
 	
-	private final String specJSON, specYAML;
+	private final String specs;
 			
-	public SwaggerController(String specJSON, String specYAML) {
+	public SwaggerController(String specs) {
 		super();
-		this.specJSON = specJSON;
-		this.specYAML = specYAML;
+		this.specs = specs;
 	}
 	
 	public SwaggerController() {
 		super();
-		this.specJSON = "swagger.json";
-		this.specYAML = "swagger.yaml";
+		this.specs = "swagger.json";		
 	}
 
+	private JsonNode getSpecs(String basePath) throws JsonProcessingException, IOException {
+		JsonNode jsonSpecs = OBJECT_MAPPER.readTree(Thread.currentThread().getContextClassLoader().getResourceAsStream(specs));		
+		((ObjectNode)jsonSpecs).put("basePath", basePath);
+		return jsonSpecs;
+	}
+	
 	@GET 
 	@Path("swagger.json")
 	@Produces(MediaType.APPLICATION_JSON)
-	public Response getJSON() {
+	public Response getJSON(@Context UriInfo uriInfo) {
 		
-		try  {		
-			return Response.ok(Thread.currentThread().getContextClassLoader().getResourceAsStream(specJSON)).build();
+		try {
+			String basePath = uriInfo.getBaseUri().getPath().toString();			
+			return Response.ok(OBJECT_MAPPER.writeValueAsString(getSpecs(basePath))).build();
 		} catch (Exception e) {
 			LOG.error("Failed to retrieve swager.json", e);
 			throw new NotFoundException(e);
@@ -67,11 +103,11 @@ public class SwaggerController {
 	@GET
 	@Path("swagger.yaml")
 	@Produces(MediaType.TEXT_PLAIN)
-	public Response getYAML() {
+	public Response getYAML(@Context UriInfo uriInfo) {
 		
-		try  {			
-			
-			return Response.ok(Thread.currentThread().getContextClassLoader().getResourceAsStream(specYAML)).build();
+		try  {
+			String basePath = uriInfo.getBaseUri().getPath().toString();			
+			return Response.ok(YAML_MAPPER.writeValueAsString(getSpecs(basePath))).build();
 		} catch (Exception e) {
 			LOG.error("Failed to retrieve swager.json", e);
 			throw new NotFoundException(e);
