318ea17a744a5e292d422659b56b4d3c2080d539
alessio.trani <a.trani@greenvulcano.com>
2017-11-22 17:27:58 +0100

    modified config.html in conformity with the dtds of GreenVulcano

--- gvengine/gvconsole/src/main/webapp/scripts/app-settings.js
+++ gvengine/gvconsole/src/main/webapp/scripts/app-settings.js
@@ -8,5 +8,23 @@ angular.module('gvconsole')
 .controller('SettingsController',['SettingsService', '$scope', function(SettingsService, $scope){
 
  $scope.service = {};
+ 
+ /* Mettere i seguenti controlli:
+  * 
+  * GreenVulcanoPool[(@maximum-size > 0 ) and 
+  * (@initial-size > @maximum-size)]}} initial-size > maximum-size
+  * 
+  * 
+  * GreenVulcanoPool[(@maximum-creation > 0) and 
+  * (@maximum-size > @maximum-creation)]}} maximum-size > maximum-creation
+  * 
+  * 
+  * ExtendedData[count(//ExtendedData[@system=current()/@system and 
+  * @service=current()/@service]) > 1]}} Attenzione: coppia sistema-servizio duplicata
+  * 
+  * 
+  * 
+  * */
+ 
 
 }]);
