2f154d427a1bf8f521b984db2e08eb04e77913a8
alessio.trani <a.trani@greenvulcano.com>
2017-11-23 11:17:59 +0100

    update settings section

--- gvengine/gvconsole/src/main/webapp/scripts/app-settings.js
+++ gvengine/gvconsole/src/main/webapp/scripts/app-settings.js
@@ -7,7 +7,34 @@ angular.module('gvconsole')
 angular.module('gvconsole')
 .controller('SettingsController',['SettingsService', '$scope', function(SettingsService, $scope){
 
- $scope.service = {};
+	
+	/*
+	 * Scegliere come prendere i valori:
+	 * 1) Prenderli tramite i forms(non tutti) e i rimanenti tramite angular.
+	 * 2) Prenderli tutti tramite angular .
+	 * 3) Modificare la sezione e far un form unico per nodo.
+	 */
+ $scope.sendCryptoHelper = function(){
+	 
+	 $scope.cryptoHelper = {
+ 
+		 
+		 type: angular.element("#CryptoHelperType").val(),
+		 name: angular.element("#CryptoHelperName").val(),
+		 description: angular.element("#CryptoHelperDescription").val(),
+		 keyStoreFolder: angular.element("#CryptoHelperKeyStoreFolder").val(),
+		 KeyStoreID: {
+			 
+			 id: angular.element("#KeyStoreIDID").val()
+				 
+		 }
+	 };
+ }
+ 
+ $scope.addKeyStoreID = function(){
+	 cryptoHelper.KeyStoreID.push({id: angular.element("#KeyStoreIDID").val()});
+ }
+ 
  
  /* Mettere i seguenti controlli:
   * 
