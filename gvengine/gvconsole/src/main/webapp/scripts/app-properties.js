76a2cf68b33d2860c8c024d0e4cb48b9486f5d35
Lorenzo Gargiullo <gargiu88@gmail.com>
2017-12-04 15:28:16 +0100

    Add modifications in section properties, style size fix, some bug fi

--- gvengine/gvconsole/src/main/webapp/scripts/app-properties.js
+++ gvengine/gvconsole/src/main/webapp/scripts/app-properties.js
@@ -1,28 +1,33 @@
 angular.module('gvconsole')
 .service('PropertiesService',['ENDPOINTS','$http', function(Endpoints, $http){
-	
+
 	this.getProperties = function(){
     	return $http.get(Endpoints.gvconfig + '/property');
     }
-	
+
 	this.getPropertyValue = function(key){
     	return $http.get(Endpoints.gvconfig + '/property/' + key);
     }
+
+	this.setProperties = function(properties){
+			return $http.put(Endpoints.gvconfig + '/property',properties,{headers:{'Content-Type':'application/json'}});
+		}
 
 }]);
 
 angular.module('gvconsole')
 .controller('PropertiesController',['PropertiesService', '$scope', function(PropertiesService, $scope){
-	
+
 	var instance = this;
 	this.properties = {};
-	
+	this.alerts = [];
+
 	PropertiesService.getProperties().then(function(response){
 		instance.properties = response.data;
 	},function(response){
 		console.log("error: " + response.data);
 	});
-	
+
 	$scope.matchKey = function(){
 		PropertiesService.getPropertyValue($scope.key).then(function(response){
 			$scope.value = response.data;
@@ -31,5 +36,13 @@ angular.module('gvconsole')
 		})
 	}
 
-
+	$scope.modifyProperties = function(){
+	PropertiesService.setProperties(instance.properties).then(function(response){
+		instance.alerts.push({type: 'success', msg: 'Properties saved'});
+		setTimeout(function(){ angular.element(".fadeout").fadeOut(); }, 3000);
+	},function(response){
+		instance.alerts.push({type: 'danger', msg: 'Properties could not be saved'});
+		setTimeout(function(){ angular.element(".fadeout").fadeOut(); }, 3000);
+	});
+};
 }]);
