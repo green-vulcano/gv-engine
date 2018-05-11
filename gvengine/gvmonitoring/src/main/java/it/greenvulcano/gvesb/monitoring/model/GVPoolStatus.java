f0c5aa1bd657077ade80a0fb049a060c5d8a2b51
santibailor <d.franco@eaisoftware.it>
2018-05-11 16:55:36 +0200

    GVPool API implementation.

--- gvengine/gvmonitoring/src/main/java/it/greenvulcano/gvesb/monitoring/model/GVPoolStatus.java
+++ gvengine/gvmonitoring/src/main/java/it/greenvulcano/gvesb/monitoring/model/GVPoolStatus.java
@@ -11,13 +11,20 @@ public class GVPoolStatus {
 	private final int maximumSize;
 	
 	private final int inUseCount;
+	
+	private final int pooledCount;
 
-	public GVPoolStatus(Instant time, String name, int maximumSize, int inUseCount) {
+	public GVPoolStatus(Instant time, String name, int maximumSize, int inUseCount, int pooledCount) {
 		super();
 		this.time = time;
 		this.name = name;
 		this.maximumSize = maximumSize;
 		this.inUseCount = inUseCount;
+		this.pooledCount = pooledCount;
+	}
+
+	public int getPooledCount() {
+		return pooledCount;
 	}
 
 	public Instant getTime() {
