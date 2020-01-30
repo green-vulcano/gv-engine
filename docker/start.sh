e29d0a12da5f2d85de0bd0a0bbe4eff20bcf1228
rolag-it <rocco.lagrotteria@gmail.com>
2020-01-30 16:37:54 +0100

    Configuration files in a seperated volume

--- docker/start.sh
+++ docker/start.sh
@@ -2,6 +2,8 @@
 
 if [ ! -d ${KARAF_HOME}/data/cache ]; then
 
+    echo "gv.app.home=${GAIA_CONFIG}/default" > ${KARAF_HOME}/etc/it.greenvulcano.gvesb.cfg
+
     sed -i -e "s|http://repo1.maven.org|https://repo1.maven.org|g" ${KARAF_HOME}/etc/org.ops4j.pax.url.mvn.cfg
     cat /opt/gaia/log.cfg >> ${KARAF_HOME}/etc/org.ops4j.pax.logging.cfg
 
