e20790bc0dcc57e4429c6a2d9dfce161e1c86ed9
rolag-it <rocco.lagrotteria@gmail.com>
2020-03-26 19:57:31 +0100

    Fix moving bin directory

--- docker/start.sh
+++ docker/start.sh
@@ -5,12 +5,12 @@ if [ ! -d ${KARAF_HOME} ]; then
     echo "Karaf home setup: $KARAF_HOME"
 
     mv /home/gaia/apache-karaf-${KARAF_VERSION} /opt/karaf
-    mv ${KARAF_HOME}/bin /home/gaia/
+
     echo "gv.app.home=/etc/gaia/default" > ${KARAF_HOME}/etc/it.greenvulcano.gvesb.cfg
 
-    sed -i -e "s|unset|echo|g" /home/gaia/bin/inc
-    sed -i -e "s|java.base=lib|java.base=${KARAF_HOME}/lib|g" /home/gaia/bin/karaf
-    sed -i -e "s|java.xml=lib|java.xml=${KARAF_HOME}/lib|g" /home/gaia/bin/karaf
+    sed -i -e "s|unset|echo|g" ${KARAF_HOME}/bin/inc
+    sed -i -e "s|java.base=lib|java.base=${KARAF_HOME}/lib|g" ${KARAF_HOME}/bin/karaf
+    sed -i -e "s|java.xml=lib|java.xml=${KARAF_HOME}/lib|g" ${KARAF_HOME}/bin/karaf
     sed -i -e "s|http://repo1.maven.org|https://repo1.maven.org|g" ${KARAF_HOME}/etc/org.ops4j.pax.url.mvn.cfg
     cat /home/gaia/log.cfg >> ${KARAF_HOME}/etc/org.ops4j.pax.logging.cfg
 
@@ -20,4 +20,5 @@ if [ ! -d ${KARAF_HOME} ]; then
     echo "DONE"
 fi
 
+cp -r ${KARAF_HOME}/bin /home/gaia/
 /home/gaia/bin/karaf server
