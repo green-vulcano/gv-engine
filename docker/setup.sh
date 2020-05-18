875d0e205f304704433b2751b53e2871db7863d7
rolag-it <rocco.lagrotteria@gmail.com>
2020-05-18 19:31:26 +0200

    Improved Docker image to support scalability

--- docker/setup.sh
+++ docker/setup.sh
@@ -5,14 +5,14 @@ public class GaiaConfigurator {
     public static void main(String[] args) throws java.io.IOException {
 
        setConfigigurationProperty("org.ops4j.pax.url.mvn.cfg", "org.ops4j.pax.url.mvn.localRepository", System.getenv("MAVEN_REPOSITORY"));
-       setConfigigurationProperty("org.ops4j.pax.url.mvn.cfg", "org.ops4j.pax.url.mvn.repositories", "http://mvn.greenvulcano.com/nexus/content/groups/public@id=gv");
+       setConfigigurationProperty("org.ops4j.pax.url.mvn.cfg", "org.ops4j.pax.url.mvn.repositories", "https://mvn.greenvulcano.com/nexus/content/groups/public@id=gv");
        setConfigigurationProperty("org.apache.karaf.features.cfg", "featuresRepositories", "mvn:it.greenvulcano.gvesb/features/"+System.getenv("GAIA_VERSION")+"/xml/features");
        setConfigigurationProperty("org.apache.karaf.features.cfg", "featuresBoot", System.getenv("GAIA_FEATURES"));
     }
 
     private static void setConfigigurationProperty(String configFileName, String key, String ... values) throws java.io.IOException  {
 
-       var configPath = java.nio.file.Paths.get(System.getenv("KARAF_HOME"), "etc", configFileName);
+       var configPath = java.nio.file.Paths.get(System.getenv("KARAF_ETC"), configFileName);
 
        var config = new java.util.Properties();
        config.load(java.nio.file.Files.newInputStream(configPath));
--- docker/start.sh
+++ docker/start.sh
@@ -3,21 +3,27 @@
 if [ ! -d ${KARAF_HOME} ]; then
 
     echo "Karaf home setup: $KARAF_HOME"
-
-    mv /home/gaia/apache-karaf-${KARAF_VERSION} /opt/karaf
-
-    echo "gv.app.home=/etc/gaia/default" > ${KARAF_HOME}/etc/it.greenvulcano.gvesb.cfg
-
+    cp -r /home/gaia/apache-karaf-${KARAF_VERSION} /opt/karaf
     sed -i -e "s|unset|echo|g" ${KARAF_HOME}/bin/inc
     sed -i -e "s|java.base=lib|java.base=${KARAF_HOME}/lib|g" ${KARAF_HOME}/bin/karaf
     sed -i -e "s|java.xml=lib|java.xml=${KARAF_HOME}/lib|g" ${KARAF_HOME}/bin/karaf
-    sed -i -e "s|http://repo1.maven.org|https://repo1.maven.org|g" ${KARAF_HOME}/etc/org.ops4j.pax.url.mvn.cfg
-    cat /home/gaia/log.cfg >> ${KARAF_HOME}/etc/org.ops4j.pax.logging.cfg
-
-    echo "Starting GAIA setup"
-    /home/gaia/setup.sh
 
     echo "DONE"
+fi
+
+if [ ! -f  ${KARAF_ETC}/it.greenvulcano.gvesb.cfg ]; then
+
+  echo "Karaf etc setup: $KARAF_ETC"
+
+  echo "gv.app.home=/etc/gaia/default" > ${KARAF_ETC}/it.greenvulcano.gvesb.cfg
+  cp -r /home/gaia/apache-karaf-${KARAF_VERSION}/etc/* $KARAF_ETC
+
+  sed -i -e "s|http://repo1.maven.org|https://repo1.maven.org|g" ${KARAF_ETC}/org.ops4j.pax.url.mvn.cfg
+  cat /home/gaia/log.cfg >> ${KARAF_ETC}/org.ops4j.pax.logging.cfg
+
+  echo "Starting GAIA setup"
+  /home/gaia/setup.sh
+
 fi
 
 cp -r ${KARAF_HOME}/bin /home/gaia/
