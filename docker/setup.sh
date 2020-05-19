baf4edf011f060d038d27b9dba3713b7c46d8b2d
rolag-it <rocco.lagrotteria@gmail.com>
2020-05-19 15:15:20 +0200

    Added option to include custom features repository

--- docker/setup.sh
+++ docker/setup.sh
@@ -7,26 +7,32 @@ public class GaiaConfigurator {
        setConfigigurationProperty("org.ops4j.pax.url.mvn.cfg", "org.ops4j.pax.url.mvn.localRepository", System.getenv("MAVEN_REPOSITORY"));
        setConfigigurationProperty("org.ops4j.pax.url.mvn.cfg", "org.ops4j.pax.url.mvn.repositories", "https://mvn.greenvulcano.com/nexus/content/groups/public@id=gv");
        setConfigigurationProperty("org.apache.karaf.features.cfg", "featuresRepositories", "mvn:it.greenvulcano.gvesb/features/"+System.getenv("GAIA_VERSION")+"/xml/features");
+       java.util.Optional.ofNullable(System.getenv("GAIA_FEATURES_REPOSITORIES"))
+                         .ifPresent(repos -> setConfigigurationProperty("org.apache.karaf.features.cfg", "featuresRepositories", repos));
        setConfigigurationProperty("org.apache.karaf.features.cfg", "featuresBoot", System.getenv("GAIA_FEATURES"));
     }
 
-    private static void setConfigigurationProperty(String configFileName, String key, String ... values) throws java.io.IOException  {
+    private static void setConfigigurationProperty(String configFileName, String key, String ... values)  {
 
-       var configPath = java.nio.file.Paths.get(System.getenv("KARAF_ETC"), configFileName);
+       try {
+           var configPath = java.nio.file.Paths.get(System.getenv("KARAF_ETC"), configFileName);
 
-       var config = new java.util.Properties();
-       config.load(java.nio.file.Files.newInputStream(configPath));
+           var config = new java.util.Properties();
+           config.load(java.nio.file.Files.newInputStream(configPath));
 
-       if (config.containsKey(key)) {
+           if (config.containsKey(key)) {
 
-       config.put(key, config.getProperty(key)
-                             .concat(",")
-                             .concat(java.util.stream.Stream.of(values).collect(java.util.stream.Collectors.joining(","))));
-       } else {
-           config.put(key, java.util.stream.Stream.of(values).collect(java.util.stream.Collectors.joining(",")));
-       }
+           config.put(key, config.getProperty(key)
+                                 .concat(",")
+                                 .concat(java.util.stream.Stream.of(values).collect(java.util.stream.Collectors.joining(","))));
+           } else {
+               config.put(key, java.util.stream.Stream.of(values).collect(java.util.stream.Collectors.joining(",")));
+           }
 
-       config.store(java.nio.file.Files.newOutputStream(configPath), "GAIA Configuration");
+           config.store(java.nio.file.Files.newOutputStream(configPath), "GAIA Configuration");
+      } catch (Exception e) {
+          e.printStackTrace();
+      }
     }
 
 }
